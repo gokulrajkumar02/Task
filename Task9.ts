df33c40d136b678dcb468e594ee66af8677a86bd
Gokul R <163151128+gokulrajkumar02@users.noreply.github.com>
2026-01-29 11:55:23 +0530

    Delete Task9.ts

--- Task9.ts
+++
@@ -1,6 +0,0 @@
-const greaterSmaller = (a:number,b:number,c : number) : number => {
-    return (a > b && a > c) ? a : ((b > a && b > c ) ? b : (c > a && c > b) ? c : 0 );
-}
-
-console.log(greaterSmaller(6,7,8))
-
