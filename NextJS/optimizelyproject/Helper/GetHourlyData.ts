b5edb60913c85a92c12548e40e0a100a05615b4a
gokulrajkumar02 <gokulrajkumar02@gmail.com>
2026-02-26 14:42:36 +0530

    Fix : type error

--- NextJS/optimizelyproject/Helper/GetHourlyData.ts
+++ NextJS/optimizelyproject/Helper/GetHourlyData.ts
@@ -2,7 +2,7 @@ import { GroupedDay } from "./GroupDays "
 
 const EMPTY_RESULT: [string[], number[]] = [[], []]
 const getHourlyData = (groupedData : GroupedDay, startTime : number) : [string[], number[]] => {
-    if(!groupedData) EMPTY_RESULT
+    if(!groupedData) return EMPTY_RESULT
     const hourlyTime = groupedData.time.slice(startTime)
     const hourlyTemp = groupedData?.apparent_temperature.slice(startTime)
     return [hourlyTime,hourlyTemp]
